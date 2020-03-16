4b07a58445d31208174fe79b7c16be749afcd341
vivascau <vlad.ivascau@gmail.com>
2020-03-16 11:43:00 +0000

    Add getters

---
+++ src/books/all.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: process.env.DYNAMODB_TABLE,
+};
+
+module.exports.all = (event, context, callback) => {
+  dynamoDb.scan(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch books.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
